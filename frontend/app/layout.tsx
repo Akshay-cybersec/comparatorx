44b81f9bc2740f1d45f5abe221c6dc9a2331e74e
Prisha0304 <prisharaut03@gmail.com>
2026-02-05 05:04:33 +0530

    toast msg added

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "react-hot-toast"; // Import Toaster
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Add the Toaster here. You can configure position/styles as needed */}
+        <Toaster position="bottom-right" reverseOrder={false} />
         {children}
       </body>
     </html>
